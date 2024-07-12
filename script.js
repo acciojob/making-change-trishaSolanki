219a1eb6f77838b1fff115ec8d14230b0b1dba00
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2024-07-12 13:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+	let change = {q:0, d:0, n:0, p:0};
+
+	change.q = Math.floor(c/25);
+	c %= 25;
+
+	change.d = Math.floor(c/10);
+	c %= 10;
+
+	change.n = Math.floor(c/5);
+	c %= 5;
+
+	change.p = c;
+	reurn change;
 };
 
 // Do not the change the code below
